6838de2653643c3c82f7f71201594e4ffc69edc0
mohammed-fedaily <mohammed.fedaily@le-campus-numerique.fr>
2025-04-07 10:55:24 +0200

    add:exo1

---
+++ Exo1/main.js
@@ -0,0 +1,9 @@
+const myBtn = document.getElementById("myBtn");
+const cntClic = document.getElementById("cntClic");
+let nbClic = 0;
+
+myBtn.addEventListener("click", () => {
+  nbClic++;
+  cntClic.textContent = `You clicked ${nbClic} times`;
+  console.log(nbClic);
+});
