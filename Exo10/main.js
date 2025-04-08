7b6da992af3cc3094209d67ff21caa436a34edf0
mohammed-fedaily <mohammed.fedaily@le-campus-numerique.fr>
2025-04-08 09:21:50 +0200

    add:exo10

---
+++ Exo10/main.js
@@ -0,0 +1,14 @@
+let check = document.getElementById("check");
+
+check.addEventListener("click", () => {
+  let year = parseInt(document.getElementById("year").value);
+  let result = document.getElementById("result");
+
+  let isBissex = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+
+  if (isBissex) {
+    result.textContent = year + " est une année bissextile";
+  } else {
+    result.textContent = year + " n'est pas une année bissextile";
+  }
+});
