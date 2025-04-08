88e904805e5bafea0463b11c71ae08bfbf8433ff
mohammed-fedaily <mohammed.fedaily@le-campus-numerique.fr>
2025-04-08 10:01:59 +0200

    add:exo11

---
+++ Exo11/main.js
@@ -0,0 +1,16 @@
+var logo = document.getElementById("logo");
+var value = 0;
+
+function animLogo() {
+  if (value < 240) {
+    value++;
+
+    logo.style.height = value + "px";
+    logo.style.width = value + "px";
+
+    let fontSize = Math.max(10, value / 10);
+    logo.style.fontSize = fontSize + "px";
+  }
+}
+
+setInterval(animLogo, 10);
